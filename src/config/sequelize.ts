0119967b1bb2b6e8dcbb7e40f350cb261b89cd8f
Mykola Petrunin <mykola.petrunin@gmail.com>
2023-09-18 14:14:54 +0200

    add pre commit

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,14 +1,20 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-const { PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT } = process.env;
+const { PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT, ENV } = process.env;
 
-if (!PGUSER || !PGPASSWORD || !PGDATABASE || !PGHOST || !PGPORT) {
+if (!PGUSER || !PGPASSWORD || !PGDATABASE || !PGHOST || !PGPORT || !ENV) {
   throw new Error('Database configuration variables are missing');
 }
 export const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
   host: PGHOST,
   port: Number(PGPORT),
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: ENV === 'prod',
+    },
+  },
   dialect: 'postgres',
   logging: false,
 });
